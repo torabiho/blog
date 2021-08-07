ccc0328635e25726ce87ed1df75d1def70b83f9b
Hossein Torabi <hossein.tb@gmail.com>
2021-08-07 18:09:41 -0400

    another test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My Personal blog ... coming soon!
+          My Personal blog ... coming soon
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
