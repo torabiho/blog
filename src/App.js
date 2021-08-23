2d5f23f39bd3c354f93ff27fce7438f21c0c7121
torabiho <hossein.tb@gmail.com>
2021-08-22 23:28:06 -0400

    fetch posts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import Page2 from "./Page2";
+import Footer from "./Footer";
 
-import Nav from './Nav';
-import Home from './Home';
-import Page2 from './Page2';
-import Footer from './Footer';
-
-function App() {
+const App = () => {
   return (
-    <div className='app'>
+    <div className="app">
       <Nav />
       <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route path='/page2' component={Page2}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/page2" component={Page2} />
       </Switch>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/Page2.js
+++ src/Page2.js
@@ -1,31 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { Trans, withTranslation } from 'react-i18next';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Trans, withTranslation } from "react-i18next";
 
-const Page2 = () => {
-  const [data, setData] = useState(null);
+const Page2 = (props) => {
+  //const { i18n } = useTranslation();
+  const [posts, setPosts] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        `${process.env.REACT_APP_API_URL}/api/posts`,
-      );
- 
-      setData(result.data);
-    };
- 
-    fetchData();
-  }, [])
+      const result = await axios(`${process.env.REACT_APP_API_URL}/api/posts`, {
+        headers: {
+          "Accept-Language": props.i18n.language,
+        },
+      });
 
-  console.log("hossein", data);
-  return (<div className='body'>
-    <p><Trans>this-is-page2</Trans></p>
-    <p>
-      <Trans i18nKey='go-to-home'>
-        <Link to='/'></Link>
-      </Trans>
-    </p>
-  </div>)
+      setPosts(result.data);
+    };
+
+    fetchData();
+  }, [props.i18n.language]);
+
+  console.log("hossein", props.i18n.language);
+  return (
+    <div className="body">
+      {posts &&
+        posts.map((post) => (
+          <li key={post._id}>
+            <p>{post[props.i18n.language]?.title}</p>
+            <p>{post[props.i18n.language]?.subtitle}</p>
+          </li>
+        ))}
+      <p>
+        <Trans>this-is-page2</Trans>
+      </p>
+      <p>
+        <Trans i18nKey="go-to-home">
+          <Link to="/"></Link>
+        </Trans>
+      </p>
+    </div>
+  );
 };
 
 export default withTranslation()(Page2);
