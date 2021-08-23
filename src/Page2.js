cb70e708c1aaabd8016d0fdafad9c8fbd6b0c5a7
torabiho <hossein.tb@gmail.com>
2021-08-23 01:49:13 -0400

    load posts

--- src/Page2.js
+++ src/Page2.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { Trans, withTranslation } from "react-i18next";
 
 const Page2 = (props) => {
-  //const { i18n } = useTranslation();
   const [posts, setPosts] = useState(null);
 
   useEffect(() => {
@@ -21,25 +20,27 @@ const Page2 = (props) => {
     fetchData();
   }, [props.i18n.language]);
 
-  console.log("hossein", props.i18n.language);
-  return (
-    <div className="body">
-      {posts &&
-        posts.map((post) => (
-          <li key={post._id}>
-            <p>{post[props.i18n.language]?.title}</p>
-            <p>{post[props.i18n.language]?.subtitle}</p>
-          </li>
-        ))}
-      <p>
-        <Trans>this-is-page2</Trans>
-      </p>
-      <p>
-        <Trans i18nKey="go-to-home">
-          <Link to="/"></Link>
-        </Trans>
-      </p>
-    </div>
+  return useMemo(
+    () => (
+      <div className="body">
+        {posts &&
+          posts.map((post) => (
+            <li key={post._id}>
+              <p>{post.title}</p>
+              <p>{post.subtitle}</p>
+            </li>
+          ))}
+        <p>
+          <Trans>this-is-page2</Trans>
+        </p>
+        <p>
+          <Trans i18nKey="go-to-home">
+            <Link to="/"></Link>
+          </Trans>
+        </p>
+      </div>
+    ),
+    [posts]
   );
 };
 
