584df80bf98c73f558bfde539e26bcb4aa3d9d37
torabiho <hossein.tb@gmail.com>
2021-12-03 00:46:55 -0500

    remove cloudinary context wrapper

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import "./App.scss";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
 import Footer from "./components/Footer";
-import { CloudinaryContext } from "cloudinary-react";
 
 const App = () => {
   useEffect(() => {
@@ -16,14 +15,14 @@ const App = () => {
   }, []);
 
   return (
-    <CloudinaryContext cloudName="dxmkio4a8" className="app">
+    <div className="app">
       <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/post/:id" component={Post} />
       </Switch>
       <Footer />
-    </CloudinaryContext>
+    </div>
   );
 };
 
