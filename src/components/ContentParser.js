91dfcb7362eeea85cb9a484e09a78c74d8005393
torabiho <hossein.tb@gmail.com>
2021-12-06 12:47:05 -0500

    Update content parser

--- src/components/ContentParser.js
+++ src/components/ContentParser.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link as ScrollLink } from "react-scroll";
 import CloudinaryGallery from "./CloudinaryGallery";
@@ -15,45 +16,43 @@ const LineParser = ({ paragraph }) => {
   const lines = paragraph.match(/.+/g);
   return (
     <p className="post__paragraph">
-      {lines.map((line, index) => (
-        <PostScriptParser
-          line={line}
-          key={index}
-          numberOfLines={lines.length}
-        />
-      ))}
+      {lines.length > 1 ? (
+        lines.map((line, index) => (
+          <React.Fragment key={index}>
+            <PostScriptParser line={line} key={index} />
+            <br />
+          </React.Fragment>
+        ))
+      ) : (
+        <PostScriptParser line={lines[0]} />
+      )}
     </p>
   );
 };
 
-const PostScriptParser = ({ line, numberOfLines }) => {
+const PostScriptParser = ({ line }) => {
   const { i18n } = useTranslation();
   const chunks = line.split(/(\[[\u06F0-\u06F9|0-9]\])/g);
 
-  return (
-    <>
-      {chunks.map((chunk, index) =>
-        /(\[[\u06F0-\u06F9|0-9]\])/.test(chunk) ? (
-          <ScrollLink
-            key={index}
-            className="postscript"
-            activeClass="current"
-            to="postscriptsRef"
-            offset={-200}
-            id={`postscript${
-              i18n.language === "fa" ? convertNumbers2English(chunk) : chunk
-            }`}
-            smooth={true}
-            duration={1500}
-          >
-            {chunk}
-          </ScrollLink>
-        ) : (
-          chunk
-        )
-      )}
-      {numberOfLines > 1 && <br />}
-    </>
+  return chunks.map((chunk, index) =>
+    /(\[[\u06F0-\u06F9|0-9]\])/.test(chunk) ? (
+      <ScrollLink
+        key={index}
+        className="postscript"
+        activeClass="current"
+        to="postscriptsRef"
+        offset={-200}
+        id={`postscript${
+          i18n.language === "fa" ? convertNumbers2English(chunk) : chunk
+        }`}
+        smooth={true}
+        duration={1500}
+      >
+        {chunk}
+      </ScrollLink>
+    ) : (
+      chunk
+    )
   );
 };
 
