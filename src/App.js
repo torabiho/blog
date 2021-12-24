bacfa2d3aa2f9baf3364e19cb220a491c8eb28bc
torabiho <hossein.tb@gmail.com>
2021-12-24 14:24:20 -0500

    remove exact from home path

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
     <div className="app">
       <Nav />
       <Switch>
-        <Route exact path="/">
+        <Route path="/">
           <Home />
         </Route>
         <Route path="/post/:id">
