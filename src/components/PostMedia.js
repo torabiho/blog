bf1e0df0b37ab6ecc8dc912968cf8beaf1097e90
torabiho <hossein.tb@gmail.com>
2022-01-17 00:04:45 -0500

    fix image max height and extension bug

--- src/components/PostMedia.js
+++ src/components/PostMedia.js
@@ -19,7 +19,7 @@ const PostMedia = ({ media, index, postId, mediaDescription }) => {
     <>
       {images.length === 1 ? (
         <div className="post__single-image__container">
-          <CloudinaryImage publicId={`${postId}/${images[0]}`} />
+          <CloudinaryImage publicId={`${postId}/${images[0]}.jpg`} />
           {mediaDescription && (
             <p className="post__single-image__caption">
               {mediaDescription[images[0]]}
